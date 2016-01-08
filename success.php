6f585cd81b469e6075e1939aba4c75570a9920b2
Luke Paireepinart <luke.paireepinart@mccoys.com>
2016-01-08 13:14:09 -0600

    added form for processing response and a thanks page to let the user know we appreciate them.

--- success.php
+++ success.php
@@ -3,6 +3,45 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Success!</p>'; ?> 
+ <?php 
+$errors = '';
+$myemail = 'luke@paireepinart.com';//<-----Put Your email address here.
+if(empty($_POST['name'])  || 
+   empty($_POST['guests']) || 
+   empty($_POST['food']))
+{
+    echo "Whoops! please fill out required fields. click <a href="/">here</a> to go back and try again.";
+}
+ 
+$name = $_POST['name']; 
+$guests = $_POST['guests']; 
+$food = $_POST['food']; 
+$comments = $_POST['comments']; 
+ 
+if( empty($errors))
+{
+ 
+$to = $myemail;
+ 
+$email_subject = "Wedding form submission: $name";
+ 
+$email_body = "You have received a new response to your invitation!\n\n".
+"Here are the details:\n".
+"    Name: $name\n".
+"    Guests: $guests\n".
+"    Food: $food\n".
+"    Comments: $comments\n";
+ 
+$headers = "From: $myemail\n";
+ 
+//$headers .= "Reply-To: $email_address";
+ 
+mail($to,$email_subject,$email_body,$headers);
+ 
+//redirect to the 'thank you' page
+ 
+header('Location: thanks.html');
+}
+ ?> 
  </body>
 </html>
