b5af8fd19b59253aa06d35b87ab4a99749cc581c
Luke Paireepinart <lukepdev@live.com>
2016-01-09 11:51:25 -0600

    some restructuring, commented out test echos, added in file write

--- submit.php
+++ submit.php
@@ -4,45 +4,29 @@
  </head>
  <body>
  <?php 
- error_reporting(E_ALL);
+
+error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-//$errors = '';
-//$myemail = 'luke@paireepinart.com';//<-----Put Your email address here.
-//if(empty($_POST['name'])  || 
-//   empty($_POST['guests']) || 
-//   empty($_POST['food']))
-//{
-//    echo "Whoops! please fill out required fields. click <a href="/">here</a> to go back and try again.";
-//}
- 
 $name = isset($_POST['name']) ? $_POST['name'] : 'No Name'; 
 $guests =  isset($_POST['guests']) ? $_POST['guests'] : 'No Guests'; 
 $food = isset($_POST['food']) ? $_POST['food'] : 'No Food'; 
 $comments = isset($_POST['comments']) ? $_POST['comments'] : 'No Comments'; 
  
-echo $name;
-
-echo $guests;
-
-echo $food;
-
-echo $comments;
-
-echo "HERE!";
-
-//if( empty($errors))
-//{
-//$email_subject = "Wedding form submission: $name";
- /*
-$email_body = "-----------------\nYou have received a new response to your invitation!\n\n".
+// echo $name;
+// echo $guests;
+// echo $food;
+// echo $comments;
+// echo "HERE!";
+$information = "-----------------\nYou have received a new response to your invitation!\n\n".
 "Here are the details:\n".
 "    Name: $name\n".
 "    Guests: $guests\n".
 "    Food: $food\n".
-"    Comments: $comments\n-------------\n";*/
- //echo "$_SERVER['DOCUMENT_ROOT']/responses.txt";
- //file_put_contents("$_SERVER['DOCUMENT_ROOT']/responses.txt", $email_body, FILE_APPEND);
+"    Comments: $comments\n-------------\n";
+$docRoot = $_SERVER['DOCUMENT_ROOT'];
+echo $docRoot;
+file_put_contents($docRoot."/responses.txt", $email_body, FILE_APPEND);
 //$headers = "From: $myemail\n";
  
 //$headers .= "Reply-To: $email_address";
