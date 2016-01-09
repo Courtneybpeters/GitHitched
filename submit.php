7ca6af6890b225809c0313b0c324b633d88c45a3
Luke Paireepinart <lukepdev@live.com>
2016-01-09 12:27:15 -0600

    added brunch to field handlers & file outputs

--- submit.php
+++ submit.php
@@ -12,6 +12,7 @@ $name = isset($_POST['name']) ? $_POST['name'] : 'No Name';
 $guests =  isset($_POST['guests']) ? $_POST['guests'] : 'No Guests'; 
 $food = isset($_POST['food']) ? $_POST['food'] : 'No Food'; 
 $comments = isset($_POST['comments']) ? $_POST['comments'] : 'No Comments'; 
+$brunch = isset($_POST['brunch']) ? $_POST['brunch'] : 'Not Attending';
 $superbowl = isset($_POST['superbowl']) ? $_POST['superbowl'] : 'Not Attending';
 $information = "You have received a new response to your invitation!\r\n\r\n".
 "Here are the details:\r\n".
@@ -19,12 +20,13 @@ $information = "You have received a new response to your invitation!\r\n\r\n".
 "    Guests: $guests\r\n".
 "    Food: $food\r\n".
 "    Comments: $comments\r\n".
+"    Brunch: $brunch\r\n".
 "    Super Bowl: $superbowl\r\n---------------------\r\n";
 $docRoot = $_SERVER['DOCUMENT_ROOT'];
 file_put_contents($docRoot."/responses.txt", $information, FILE_APPEND);
 
 $csvFile = fopen("responses.csv", "a");
-fputcsv($csvFile, [$name, $guests, $food, $comments, $superbowl]);
+fputcsv($csvFile, [$name, $guests, $food, $comments, $superbowl, $brunch]);
 fclose($csvFile);
 
 //redirect to the 'thank you' page
