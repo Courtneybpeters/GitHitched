5c179f83fafc8ed933afa617a624be85c5418416
Luke Paireepinart <lukepdev@live.com>
2016-01-09 12:10:14 -0600

    not attending label updated.

--- submit.php
+++ submit.php
@@ -12,7 +12,7 @@ $name = isset($_POST['name']) ? $_POST['name'] : 'No Name';
 $guests =  isset($_POST['guests']) ? $_POST['guests'] : 'No Guests'; 
 $food = isset($_POST['food']) ? $_POST['food'] : 'No Food'; 
 $comments = isset($_POST['comments']) ? $_POST['comments'] : 'No Comments'; 
-$superbowl = isset($_POST['superbowl']) ? $_POST['superbowl'] : 'No SuperBowl Preference';
+$superbowl = isset($_POST['superbowl']) ? $_POST['superbowl'] : 'Not Attending';
 $information = "You have received a new response to your invitation!\r\n\r\n".
 "Here are the details:\r\n".
 "    Name: $name\r\n".
