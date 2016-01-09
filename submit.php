3c57d20a1ce754e1906373dd841c7effc6028891
Luke Paireepinart <lukepdev@live.com>
2016-01-09 12:05:32 -0600

    added attendance information for super bowl party

--- submit.php
+++ submit.php
@@ -12,18 +12,19 @@ $name = isset($_POST['name']) ? $_POST['name'] : 'No Name';
 $guests =  isset($_POST['guests']) ? $_POST['guests'] : 'No Guests'; 
 $food = isset($_POST['food']) ? $_POST['food'] : 'No Food'; 
 $comments = isset($_POST['comments']) ? $_POST['comments'] : 'No Comments'; 
- 
-$information = "-----------------\r\nYou have received a new response to your invitation!\r\n\r\n".
+$superbowl = isset($_POST['superbowl']) ? $_POST['superbowl'] : 'No SuperBowl Preference';
+$information = "You have received a new response to your invitation!\r\n\r\n".
 "Here are the details:\r\n".
 "    Name: $name\r\n".
 "    Guests: $guests\r\n".
 "    Food: $food\r\n".
-"    Comments: $comments\r\n-------------\r\n";
+"    Comments: $comments\r\n".
+"    Super Bowl: $superbowl\r\n---------------------\r\n";
 $docRoot = $_SERVER['DOCUMENT_ROOT'];
 file_put_contents($docRoot."/responses.txt", $information, FILE_APPEND);
 
 $csvFile = fopen("responses.csv", "a");
-fputcsv($csvFile, [$name, $guests, $food, $comments]);
+fputcsv($csvFile, [$name, $guests, $food, $comments, $superbowl]);
 fclose($csvFile);
 
 //redirect to the 'thank you' page
